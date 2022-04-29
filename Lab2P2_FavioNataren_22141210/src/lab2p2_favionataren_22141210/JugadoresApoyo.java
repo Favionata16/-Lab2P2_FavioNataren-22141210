38931a5bec9d1dd777ed1652fa3bb6d402d164e3
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-04-29 13:53:02 -0600

    commit 1

---
+++ Lab2P2_FavioNataren_22141210/src/lab2p2_favionataren_22141210/JugadoresApoyo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_favionataren_22141210;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author elbal
+ */
+public class JugadoresApoyo {
+    Scanner leer = new Scanner (System.in).useDelimiter("\n");
+    ArrayList<jugadores> jugadoresLista = new ArrayList<jugadores>();
+    
+    //Constructor
+    public JugadoresApoyo(){
+    }
+    
+    
+
+    
+
+    
+}
---
+++ Lab2P2_FavioNataren_22141210/src/lab2p2_favionataren_22141210/jugadores.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_favionataren_22141210;
+
+/**
+ *
+ * @author elbal
+ */
+public class jugadores {
+        
+    //Atributos
+    String nombre;
+    
+    //Constructor
+    public jugadores(String nombre){
+            
+    }
+    
+    //Funciones
+    
+    //Función para imprimir los atributos
+    public void imprimir(){
+        System.out.println("[Nombre: "+nombre+"]");
+    }
+    
+}
