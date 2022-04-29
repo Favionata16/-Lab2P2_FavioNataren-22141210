0f982020e38f648b9de3103752da9c0c06bffe43
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-04-29 14:06:36 -0600

    commit 2

---
+++ Lab2P2_FavioNataren_22141210/src/lab2p2_favionataren_22141210/Lab2P2_FavioNataren_22141210/src/lab2p2_favionataren_22141210/juego.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_favionataren_22141210;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elbal
+ */
+public class juego {
+
+
+}
